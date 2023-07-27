d91c15a95d60e45e6464f15311283dd0d4b179d2
Andrew Sun <tyriongump@gmail.com>
2023-07-28 09:55:04 +1000

    Temporarily save failed events locally

--- hermes-consumer-vicroad/src/main/java/org/bugmakers404/hermes/consumer/vicroad/service/S3ClientMockServiceImpl.java
+++ hermes-consumer-vicroad/src/main/java/org/bugmakers404/hermes/consumer/vicroad/service/S3ClientMockServiceImpl.java
@@ -1,5 +1,9 @@
 package org.bugmakers404.hermes.consumer.vicroad.service;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -16,7 +20,24 @@ public class S3ClientMockServiceImpl implements FailedEventsArchiveService {
 
   @Override
   public void archiveFailedEvent(@NonNull String topic, @NonNull String key, String event) {
+    String filePath = Constants.BLUETOOTH_DATA_ARCHIVES_EVENT_PATH.formatted(topic, key);
+    storeEventsToLocalFiles(topic, filePath, event);
+
     log.info("{} - Succeed to archive the failed event with key {} in S3 bucket {}", topic,
         key, Constants.HERMES_DATA_BUCKET_NAME);
+  }
+
+  private void storeEventsToLocalFiles(String topic, String filePath, String content) {
+
+    Path targetPath = Paths.get(filePath);
+
+    try {
+      Files.createDirectories(targetPath.getParent());
+      Files.writeString(targetPath, content);
+      log.info("{} - Succeed to archive the non-persistent event locally at {}", topic, filePath);
+    } catch (IOException e) {
+      log.error("{} - Failed to archive the non-persistent event locally: {}", topic,
+          e.getMessage(), e);
+    }
   }
 }
